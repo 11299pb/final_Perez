0e5ba03ac9416732cb148af6b8e3aa8c5a4064d3
paola <pebitaa5795@hotmail.com>
2023-06-12 11:12:00 -0600

    CONTROLADORES

--- vistas/pacientes/modificar.php
+++ vistas/pacientes/modificar.php
@@ -13,25 +13,25 @@ require '../../modelos/Paciente.php';
 ?>
 <?php include_once '../../includes/header.php'?>
     <div class="container">
-        <h1 class="text-center">Modificar pacientes</h1>
+        <h1 class="text-center">MODIFICACION DE PACIENTES</h1>
         <div class="row justify-content-center">
             <form action="/final_perez/controladores/pacientes/modificar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <input type="hidden" name="medico_id">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="paci_nombre">Nombre del paciente</label>
+                        <label for="paci_nombre">NOMBRE DEL PACIENTE</label>
                         <input type="text" name="paci_nombre" id="paci_nombre" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="paci_dpi">No. de DPI</label>
+                        <label for="paci_dpi">NUMERO DE DPI</label>
                         <input type="text" name="paci_dpi" id="paci_dpi" class="form-control" required>
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="paci_telefono">No. de telefono</label>
+                        <label for="paci_telefono">NUMERO DE TELEFONO</label>
                         <input type="text" name="paci_telefono" id="paci_telefono" class="form-control" required>
                     </div>
                 </div>
