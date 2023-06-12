1fa19df32694df5dc710eac9cb50c0afaf040c0a
paola <pebitaa5795@hotmail.com>
2023-06-12 00:31:52 -0600

    se realizo archivo buscar

--- bd.sql
+++ bd.sql
@@ -1,15 +1,5 @@
-create table especialidad(
-    esp_id serial not null,
-    esp_nombre varchar(35) not null,
-    esp_situacion smallint not null default 1,
-    primary key (esp_id)
-)
-create table clinicas(
-    clin_id serial not null,
-    clin_nombre varchar(35) not null,
-    clin_situacion smallint not null default 1,
-    primary key (clin_id)
-)
+create database final
+
 create table pacientes(
     paci_id serial not null,
     paci_nombre varchar(25) not null,
@@ -18,6 +8,20 @@ create table pacientes(
     paci_situacion smallint not null default 1,
     primary key (paci_id)
 )
+
+create table especialidad(
+    esp_id serial not null,
+    esp_nombre varchar(35) not null,
+    esp_situacion smallint not null default 1,
+    primary key (esp_id)
+);
+create table clinicas(
+    clin_id serial not null,
+    clin_nombre varchar(35) not null,
+    clin_situacion smallint not null default 1,
+    primary key (clin_id)
+)
+
 create table medicos(
     med_id serial not null,
     med_nombre varchar(25) not null,
@@ -28,17 +32,17 @@ create table medicos(
     foreign key (med_especialidad) REFERENCES especialidad (esp_id),
     foreign key (med_clinica) REFERENCES clinicas (clin_id)
 )
-create table citas(
-    cit_id serial not null,
-    cit_paciente integer not null,
-    cit_medico integer not null,
-    cit_fecha DATETIME YEAR TO MINUTE NOT NULL,
-    cit_hora TIME HOUR TO MINUTE NOT NULL,
-    cit_referencia varchar(5) not null,
-    cit_situacion smallint not null default 1,
-    primary key (cit_id),
-    foreign key (cit_paciente) REFERENCES pacientes (paci_id),
-    foreign key (cit_medico) REFERENCES medicos (med_id)
+CREATE TABLE citas (
+    cit_id SERIAL NOT NULL,
+    cit_paciente INTEGER NOT NULL,
+    cit_medico INTEGER NOT NULL,
+    cit_fecha DATE NOT NULL,
+    cit_hora INTERVAL HOUR TO MINUTE NOT NULL,
+    cit_referencia VARCHAR(5) NOT NULL,
+    cit_situacion SMALLINT NOT NULL DEFAULT 1,
+    PRIMARY KEY (cit_id),
+    FOREIGN KEY (cit_paciente) REFERENCES pacientes (paci_id),
+    FOREIGN KEY (cit_medico) REFERENCES medicos (med_id)
 )
 create table recetas(
     rec_id serial not null,
@@ -46,15 +50,15 @@ create table recetas(
     rec_medicamentos varchar(50) not null,
     rec_situacion smallint not null default 1,
     primary key (rec_id),
-    foreign key (rec_cita) REFERENCES citas (cita_id)
-)
+    foreign key (rec_cita) REFERENCES citas (cit_id)
+);
 create table diagnosticos(
     diag_id serial not null,
     diag_cita integer not null,
     diag_descripcion varchar(100) not null,
     diag_situacion smallint not null default 1,
     primary key (diag_id),
-    foreign key (diag_cita) REFERENCES citas (cita_id)
+    foreign key (diag_cita) REFERENCES citas (cit_id)
 )
 create table detalles(
     det_id serial not null,
