0894ddb5e92bce6d79fb0daeadb7b2ba3fbfd127
paola <pebitaa5795@hotmail.com>
2023-06-12 11:37:31 -0600

    MODIFICACION DE CARPETA MEDICOS

--- vistas/clinicas/buscar.php
+++ vistas/clinicas/buscar.php
@@ -1,21 +1,33 @@
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Buscar Medicos</h1>
+        <br><h1 class="text-center" style="font-family: Arial, sans-serif; color: #4c586f;">BUSCAR DE CLINICAS</h1><br>
         <div class="row justify-content-center">
-            <form action="/final_perez/controladores/clinicas/buscar.php" method="GET" class="col-lg-8 border bg-light p-3">
+            <form action="/final_perez/controladores/clinicas/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="clin_nombre">Nombre de la Clinica</label>
+                        <label for="clin_nombre" style="font-family: Arial, sans-serif; color: #4c586f; font-weight: bold;">Nombre de la Clínica</label>
                         <input type="text" name="clin_nombre" id="clin_nombre" class="form-control">
                     </div>
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <button type="submit" class="btn btn-info w-100">Buscar</button>
+                        <button type="submit" class="btn btn-primary w-100" style="background-color: #4c586f; border-color: #4c586f; font-family: Arial, sans-serif;">Guardar</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
-    <?php include_once '../../includes/footer.php'?>
+<?php include_once '../../includes/footer.php'?>
+<style>
+    body {
+        background-color: #f8f9fa;
+    }
+    .form-control {
+        border-color: #ced4da;
+    }
+    .form-control:focus {
+        border-color: #80bdff;
+        box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+</style>
--- vistas/medicos/index.php
+++ vistas/medicos/index.php
@@ -4,28 +4,26 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once '../../modelos/Especialidad.php';
 require_once '../../modelos/Clinica.php';
-    try {
-        $especialidad = new Especialidad();
-        $clinica = new Clinica();
-        $especialidades = $especialidad->buscar();
-        $clinicas = $clinica->buscar();
-            // var_dump($clientes);
-            // exit;
-    } catch (PDOException $e) {
-        $error = $e->getMessage();
-    } catch (Exception $e2){
-        $error = $e2->getMessage();
-    }
+try {
+    $especialidad = new Especialidad();
+    $clinica = new Clinica();
+    $especialidades = $especialidad->buscar();
+    $clinicas = $clinica->buscar();
+} catch (PDOException $e) {
+    $error = $e->getMessage();
+} catch (Exception $e2) {
+    $error = $e2->getMessage();
+}
 ?>
 <?php include_once '../../includes/header.php'?>
 <?php include_once '../../includes/navbar.php'?>
     <div class="container">
-        <h1 class="text-center">Formulario de ingreso de Medicos</h1>
+        <h1 class="text-center" style="font-family: 'Arial', sans-serif; color: #333;">Formulario de ingreso de Médicos</h1>
         <div class="row justify-content-center">
-            <form action="/final_perez/controladores/medicos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3">
+            <form action="/final_perez/controladores/medicos/guardar.php" method="POST" class="col-lg-8 border bg-light p-3" style="font-family: 'Arial', sans-serif; color: #333;">
                 <div class="row mb-3">
                     <div class="col">
-                    <label for="med_nombre">Nombre del medico</label>
+                        <label for="med_nombre">Nombre del médico</label>
                         <input type="text" name="med_nombre" id="med_nombre" class="form-control">
                     </div>
                 </div>
@@ -42,7 +40,7 @@ require_once '../../modelos/Clinica.php';
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <label for="med_clinica">Clinica</label>
+                        <label for="med_clinica">Clínica</label>
                         <select name="med_clinica" id="medico_clinica" class="form-control">
                             <option value="">SELECCIONE...</option>
                             <?php foreach ($clinicas as $key => $clinica) : ?>
@@ -53,10 +51,22 @@ require_once '../../modelos/Clinica.php';
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        <button type="submit" class="btn btn-primary w-100">Guardar</button>
+                        <button type="submit" class="btn btn-primary w-100" style="background-color: #337ab7; border-color: #337ab7;">Guardar</button>
                     </div>
                 </div>
             </form>
         </div>
     </div>
 <?php include_once '../../includes/footer.php'?>
+<style>
+    body {
+        background-color: #f8f9fa;
+    }
+    .form-control {
+        border-color: #ced4da;
+    }
+    .form-control:focus {
+        border-color: #80bdff;
+        box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
+    }
+</
